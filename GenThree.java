d37758de685017a193b6a8995c0c91db35a04d23
Anan-G <anan.gara@post.runi.ac.il>
2023-12-21 18:38:32 +0200

    This is my first commit.

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,17 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int range = b-a; 
+		int randomInt1 = (int)(Math.random()*range) + a;
+		int randomInt2 = (int)(Math.random()*range) + a;
+		int randomInt3 = (int)(Math.random()*range) + a;
+		int min1 = Math.min(randomInt1, randomInt2);
+		int min2 = Math.min(randomInt3, min1);
+		System.out.println(randomInt1);
+		System.out.println(randomInt2);
+		System.out.println(randomInt3);
+		System.out.println("The minimal generated number was " + min2);
 	}
 }
